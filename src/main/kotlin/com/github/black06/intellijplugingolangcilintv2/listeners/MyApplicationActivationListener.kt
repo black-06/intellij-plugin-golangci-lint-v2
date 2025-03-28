8befa73291203ea4692a0cf54f2c1878310e5929
GitHub Action <action@github.com>
2025-03-28 06:27:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/black06/intellijplugingolangcilintv2/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/black06/intellijplugingolangcilintv2/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.black06.intellijplugingolangcilintv2.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
